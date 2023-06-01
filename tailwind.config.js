5a45b80c44f163f28da4ae758134d4a8f11f1483
Angus <angus.goody@gmail.com>
2023-06-01 14:43:44 +0100

    Added colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,40 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+
+            colors: {
+                primary: {
+                    light2: "#BBF48A",
+                    light: "#91DC52",
+                    DEFAULT: "#62b31e",
+                    dark: "#559B1B",
+                    dark2: "#4D861E",
+                },
+
+                accent: {
+                    light2: "#7DBFF0",
+                    light: "#509ED8",
+                    DEFAULT: "#3087C7",
+                    dark: "#347CB1",
+                    dark2: "#2C6A98",
+                },
+
+                dark: {
+                    light2: "#4A6682",
+                    light: "#344D66",
+                    DEFAULT: "#24384C",
+                    dark: "#253442",
+                    dark2: "#24313D",
+                },
+
+                light: {
+                    light2: "#FFFFFF",
+                    light: "#FAFAFA",
+                    DEFAULT: "#F3F3F3",
+                    dark: "#DEDEDE",
+                    dark2: "#D0D0D0",
+                },
+            },
         },
     },
 
