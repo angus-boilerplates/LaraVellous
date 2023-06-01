8c59a53cdf76b63e72eee576c6fe4078ccec1af9
Angus <angus.goody@gmail.com>
2023-06-01 22:23:22 +0100

    Added role class and admins

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+//Models
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+      $superAdminRole=Role::where('name', '=', 'Super Admin')->firstOrFail();
+      if(config('admin.admin_name')) {
+        User::create([
+          'name' => config('admin.admin_name'),
+          'email' => config('admin.admin_email'),
+          'role_id' => $superAdminRole->id,
+          'password' => Hash::make(config('admin.admin_password')),
+        ]);
+      }
+
+    }
+}
