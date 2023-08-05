536860eea4c66d50c803e6881e56b5e6bf5bf5e6
Angus Goody <angus.goody@gmail.com>
2023-08-05 09:43:27 +0100

    Ci update (#6)
    * Added new files for ci/cd pipeline
    
    * updated readme.md
    
    * tweaks
    
    * tweaking other parts
    
    * cache tweaks
    
    * simplified cache
    
    * actions are triggered manually

---
+++ start_prod.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+# Create .env file from environment variables
+printenv | awk -F "=" 'NF==2 && $2 !~ /[\n\t ]/' > .env
+
+# Run our artisan commands
+php artisan route:clear
+php artisan config:clear
+php artisan view:clear
+
+php artisan storage:link
+
+php artisan migrate --force
+chmod -R 777 storage
+
+# Finally, start PHP-FPM and nginx
+php-fpm -D &&  nginx -g "daemon off;"
